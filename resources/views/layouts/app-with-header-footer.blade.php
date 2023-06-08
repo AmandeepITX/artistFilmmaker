6498e59bd8ea98f2aba45844125e9c4be27a3830
nikhiljha647 <nikhiljha647@gmail.com>
2023-06-08 16:10:46 +0530

    update

--- resources/views/layouts/app-with-header-footer.blade.php
+++ resources/views/layouts/app-with-header-footer.blade.php
@@ -11,6 +11,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
         crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    
     <!-- slider-link -->
     <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
     <!-- slider-link-end -->
--- resources/views/pages/common/profilePage.blade.php
+++ resources/views/pages/common/profilePage.blade.php
@@ -2,6 +2,9 @@
 @section('title', 'Profile')
 @section('content')
 <div class="profile-titel">
+    <a class="back-btn">
+<i class="fa-solid fa-arrow-left"></i>
+    </a>
         <div class="container">
             <h2>Profile</h2>
         </div>
